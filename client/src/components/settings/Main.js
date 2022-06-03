61898551d979a4f787a88340ae0bf61429c4f269
lorazhoub <lorazhoub@gmail.com>
2022-06-02 22:04:53 -0700

    Basic Video Chat Settings Box for Settings

---
+++ client/src/components/settings/Main.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import VideoChatSettings from './VideoChatSettings';
+
+
+const theme = {
+    spacing: 2
+}
+
+function Main () {
+    return (
+        <div>
+            <VideoChatSettings />
+        </div>
+    );
+}
+  
+export default Main;
