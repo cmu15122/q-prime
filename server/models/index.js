69885de63fb98cb64f18359dd5abca0e24f8ddf6
cosmic20 <bhargavhadya@gmail.com>
2022-06-08 23:18:03 -0700

    Added types for all models, working on association

--- server/models/index.js
+++ server/models/index.js
@@ -35,3 +35,144 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+//Tables
+
+module.exports = (sequelize, DataTypes) => {
+	var Assignments = sequelize.define('Assignments',{
+		// assignment_id: {
+		// 	type: DataTypes.INTEGER,
+		// 	autoincrement: true,
+		// 	primaryKey: true
+		// },
+		name:{
+			type: DataTypes.STRING //varchar(255)	
+		},
+		category:{
+			type: DataTypes.STRING
+		}
+	});
+
+	var Sem = sequelize.define('Semesters', {
+		sem_id:{
+			type: DataTypes.STRING,
+			unique: true,
+			defaultValue: false,
+			primaryKey: true
+		}
+	});
+
+
+
+	var Assign_Sem = sequelize.define('Assignments_Semesters',{
+		start_date:{
+			type: DataTypes.DATE,
+			allowNull: false
+		},
+		end_date:{
+			type: DataTypes.DATE,
+			allowNull: false
+		},
+		assignmentId:{
+			type: DataTypes.INTEGER,
+			references:{
+				model: Assignments,
+				key: 'id'
+			}
+		},
+		semesterId:{
+			type: DataTypes.INTEGER,
+			references:{
+				model: Semesters,
+				key: 'sem_id'
+			}
+		}
+	});
+
+	Assignments.belongsToMany(Semesters, {through: Assign_Sem, uniqueKey: 'id'});
+	Semesters.belongsToMany(Assignments, {through: Assign_Sem, uniqueKey: 'sem_id' });
+
+
+	var Users = sequelize.define('Users', {
+		// user_id:{
+		// 	type: DataTypes.INTEGER,
+		// 	autoincrement:true,
+		// 	primaryKey:true
+		// },
+		email:{
+			type: DataTypes.STRING,
+			defaultValue: false,
+			unique: true
+		},
+		fname:{
+			type: DataTypes.STRING,
+			defaultValue: false
+		},
+		lname:{
+			type: DataTypes.STRING,
+			defaultValue: false
+		},
+		access_token:{
+			type: DataTypes.STRING,
+			defaultValue: false
+		}
+	});
+
+	var Sem_users = sequelize.define('Semesters_Users', {
+
+	});
+
+	var Students = sequelize.define('Students', {
+		num_questions:{
+			type: DataTypes.BIGINT
+		},
+		time_on_queue:{
+			type: DataTypes.BIGINT //minutes??
+		}
+	});
+
+
+	var TA = sequelize.define('TAs', {
+		is_admin:{
+			type: DataType.BOOLEAN
+		},
+		zoom_url:{
+			type: DataTypes.STRING
+		},
+		num_helped:{
+			type: DataTypes.BIGINT
+		},
+		time_helped:{
+			type: DataTypes.BIGINT
+		}
+	});
+
+	var Questions = sequelize.define('Questions', {
+		question:{
+			types:DataTypes.TEXT
+		},
+		tried_so_far:{
+			types:DataTypes.TEXT
+		},
+		location:{
+			type: DataTypes.STRING,
+			defaultValue:false
+		},
+		assignment:{
+			type: DataTypes.STRING,
+			defaultValue:false
+		},
+		entry_time:{
+			type: DataTypes.DATE
+		},
+		help_time:{
+			type: DataTypes.DATE
+		},
+		exit_time:{
+			type: DataTypes.DATE
+		},
+		num_ask_to_fix:{
+			type: DataTypes.INTEGER
+		}
+	})
+};
