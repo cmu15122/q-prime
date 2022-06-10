8b1cf347a23a53914a6edbaf6793908eec6a603a
lorazhoub <lorazhoub@gmail.com>
2022-06-09 20:37:59 -0700

    Added date/time picker for Metrics page

--- client/src/components/metrics/MetricsMain.js
+++ client/src/components/metrics/MetricsMain.js
@@ -3,11 +3,13 @@ import LogoutNavbar from '../navbar/LogoutNavbar';
 import LoginStudentNavbar from '../navbar/LoginStudentNavbar';
 import LoginTANavbar from '../navbar/LoginTANavbar';
 import LoginAdminNavbar from '../navbar/LoginAdminNavbar';
+import DateTimeSelector from './DateTimeSelector';
 
 function MetricsMain (props) {
     return (
         <div>
             <LoginAdminNavbar theme={props.theme}/>
+            <DateTimeSelector theme={props.theme}/>
         </div>
     );
 }
