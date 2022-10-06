af9c6675c57415205d9b20c6ab4e1612960041f5
Yutian Chen <47029019+MarkChenYutian@users.noreply.github.com>
2022-10-06 14:05:44 -0400

    Add Change name button & Fix npm start issue (#168)
    * :sparkles: LoggedInAs now shows preferred_name instead of AndrewID and
    fix npm start to support later version of npm
    
    * :sparkles: Add change name button for issue #159
    
    * :sparkles: Change 'Change Name' button to icon
    
    Co-authored-by: Yutian Chen <markchenyutian@gmail.com>

---
+++ client/src/components/navbar/changeNameBtn.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import {
+    Button, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText, TextField,
+} from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+
+import SettingsService from "../../services/SettingsService";
+
+export default function ChangeNameBtn(props) {
+    const { queueData, setpname, pname } = props;
+    const [ tmp_pname, setTmppname] = useState(pname);
+    const [ open, setOpen ] = useState(false);
+    
+    useEffect(() => {
+        if (queueData != null) {
+            setpname(queueData.preferred_name);
+        }
+    }, [queueData, setpname]);
+    
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        SettingsService.updatePreferredName({preferred_name:tmp_pname});
+        handleClose();
+    };
+    
+    return (
+        <div>
+            <Button variant="h8" onClick={handleClickOpen} sx={{ color: "#FFFFFF" }}>
+                <EditIcon/>
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <form onSubmit={handleSubmit}>
+                    <DialogTitle>Change Name</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Update the name that is displayed when you are on the queue and the
+                            name we will call out to help you during office hours. Your
+                            professor can see this, so please make it appropriate!
+                        </DialogContentText>
+                        <TextField
+                            value={tmp_pname}
+                            onChange={(e) => setTmppname(e.target.value)}
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Preferred Name"
+                            type="pname"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button type="submit" onClick={() => setpname(tmp_pname)}>Set Nickname</Button>
+                    </DialogActions>
+                </form>
+            </Dialog>
+        </div>
+    );
+}
