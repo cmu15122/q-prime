bad23d8ea1c36ec1daa950e44bfb574d939f3076
Angela Zhang <angelaz1@andrew.cmu.edu>
2022-10-19 01:38:24 +0100

    Moving changes to .tsx files

--- client/src/http-common.tsx
+++ client/src/http-common.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import {toast} from 'material-react-toastify';
 import Cookies from 'universal-cookie';
+
+import { showErrorToast } from "./services/ToastService";
 
 const cookies = new Cookies();
 
@@ -12,55 +13,41 @@ const httpInstance = axios.create({
 });
 
 httpInstance.interceptors.request.use(
-    (req) => {
-      const userCookies = cookies.get('user');
-      if (userCookies != null) {
-        req.headers['Authorization'] = userCookies['access_token'];
-      }
-      return req;
-    },
-    (err) => {
-      return Promise.reject(err);
-    },
+  (req) => {
+    const userCookies = cookies.get('user');
+    if (userCookies != null) {
+      req.headers['Authorization'] = userCookies['access_token'];
+    }
+    return req;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
 );
 
 httpInstance.interceptors.response.use(
-    (res) => {
-      if (res.data.isOwner && !window.location.href.includes('settings')) {
-        // Redirect owner to settings page
-        window.location.href = '/settings';
-      }
-      // COMMENTED OUT SO ONLY TOAST IF ERROR
-      // if (res.data.message) {
-      //     toast(res.data.message, {
-      //         position: "bottom-left",
-      //         hideProgressBar: false,
-      //         closeOnClick: true,
-      //         pauseOnHover: true,
-      //         draggable: true,
-      //         progress: undefined,
-      //     });
-      // }
-      return res;
-    },
-    (err) => {
-      if (err.response.status === 404) {
-        // Redirect to homepage
-        window.location.href = '/';
-      }
-      if (err.response.data.message) {
-        const message = err.message + ': ' + err.response.data.message;
-        toast.error(message, {
-          position: 'bottom-left',
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }
-      return Promise.reject(err);
-    },
+  (res) => {
+    if (res.data.isOwner && !window.location.href.includes('settings')) {
+      // Redirect owner to settings page
+      window.location.href = '/settings';
+    }
+    // COMMENTED OUT SO ONLY TOAST IF ERROR
+    // if (res.data.message) {
+    //     toastService.showToast(res.data.message);
+    // }
+    return res;
+  },
+  (err) => {
+    if (err.response.status === 404) {
+      // Redirect to homepage
+      window.location.href = '/';
+    }
+    if (err.response.data.message) {
+      const message = err.message + ': ' + err.response.data.message;
+      showErrorToast(message);
+    }
+    return Promise.reject(err);
+  },
 );
 
 export default httpInstance;
