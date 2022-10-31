ca567490929ba8322a1a06912d6b442f1c87c03d
Angela Zhang <angelaz1@andrew.cmu.edu>
2022-10-31 14:13:28 -0400

    Catching missing semester error

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -60,67 +60,85 @@ exports.post_login = async (req, res) => {
         }
 
         Promise.props({
-            account: models.account.findOrCreate({ 
-                where: { email: email }
-            }),
-            name: name
-        }).then((res) => {
-            let name = res.name;
-            let [account, created] = res.account;
-    
-            if (created || !account.access_token) {
-                const access_token = jwt.sign(
-                    {
-                        name: name,
-                        email: email
-                    },
-                    config.TOKEN_KEY,
-                    { algorithm: 'HS256' }
-                );
-    
-                account.set({
-                    name: name,
-                    preferred_name: name,
-                    access_token: access_token
-                });
+            semester: models.semester.findByPk(adminSettings.currSem),
+        }).then((result) => {
+            if (result.semester == null) {
+                res.status(500);
+                res.json({ message: "Queue has not been initialized; must log in with owner email" });
+                return;
             }
-    
-            return Promise.props({
-                account: account.save(),
-                semUser: models.semester_user.findOrCreate({
-                    where: {
-                        sem_id: adminSettings.currSem,
-                        user_id: account.user_id
-                    }
-                })
-            });
-        }).then(function(results) {
-            let account = results.account;
-            let [semUser, semUserCreated] = results.semUser;
-    
-            if (semUserCreated || (semUser.isTA != 1)) {
+
+            Promise.props({
+                account: models.account.findOrCreate({ 
+                    where: { email: email }
+                }),
+                name: name
+            }).then((res) => {
+                let name = res.name;
+                let [account, created] = res.account;
+        
+                if (created || !account.access_token) {
+                    const access_token = jwt.sign(
+                        {
+                            name: name,
+                            email: email
+                        },
+                        config.TOKEN_KEY,
+                        { algorithm: 'HS256' }
+                    );
+        
+                    account.set({
+                        name: name,
+                        preferred_name: name,
+                        access_token: access_token
+                    });
+                }
+        
+                return Promise.props({
+                    account: account.save(),
+                    semUser: models.semester_user.findOrCreate({
+                        where: {
+                            sem_id: adminSettings.currSem,
+                            user_id: account.user_id
+                        }
+                    })
+                });
+            }).then(function(results) {
+                let account = results.account;
+                let [semUser, semUserCreated] = results.semUser;
+        
+                if (semUserCreated || (semUser.isTA != 1)) {
+                    return Promise.props({
+                        account: account,
+                        student: function() {
+                            return models.student.findOrCreate({
+                                where: {
+                                    student_id: account.user_id
+                                }
+                            });
+                        }(),
+                    });
+                }
+        
                 return Promise.props({
                     account: account,
-                    student: function() {
-                        return models.student.findOrCreate({
-                            where: {
-                                student_id: account.user_id
-                            }
-                        });
+                    ta: function() {
+                        return models.ta.findByPk(account.user_id);
                     }(),
                 });
-            }
-    
-            return Promise.props({
-                account: account,
-                ta: function() {
-                    return models.ta.findByPk(account.user_id);
-                }(),
+            }).then(function(results) {
+                let account = results.account;
+                res.status(201);
+                res.json({ name: account.name, email: account.email, access_token: account.access_token });
+            }).catch(function(err) {
+                console.log(err);
+                res.status(500);
+                res.json({ message: err.message });
             });
-        }).then(function(results) {
-            let account = results.account;
-            res.status(201);
-            res.json({ name: account.name, email: account.email, access_token: account.access_token });
+        }).catch(function(err) {
+            console.log(err);
+            res.status(500);
+            res.json({ message: err.message });
         });
     }).catch(function(err) {
         console.log(err);
