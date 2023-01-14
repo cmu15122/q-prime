961f059c28c1c8b5cc86e9bd2d91a805b09fbaaa
Jackson Romero <jacksontromero@gmail.com>
2023-01-13 23:02:02 -0500

    Remove unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
-import React, {createContext, useContext, useEffect, useState} from 'react';
-import {BrowserRouter as Router, Routes, Route}
-  from 'react-router-dom';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/home';
 import Settings from './pages/settings';
