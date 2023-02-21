2f80432bb173692afbd6d1e7dd85540da847f42d
Jackson Romero <jacksontromero@gmail.com>
2023-02-21 00:22:16 -0500

    Add descrpitions for Contexts

--- client/src/contexts/AllStudentsContext.tsx
+++ client/src/contexts/AllStudentsContext.tsx
@@ -4,15 +4,26 @@ import {UserDataContext} from '../contexts/UserDataContext';
 import HomeService from '../services/HomeService';
 import {socketSubscribeTo} from '../services/SocketsService';
 
+/**
+ * Context object for all students on the queue
+ *
+ * This context will only be loaded for users who are TAs
+ *
+ * This context is updated via the `allStudents` socket
+ */
 const AllStudentsContext = createContext({
   allStudents: [] as StudentData[],
   setAllStudents: ((allStudents: StudentData[]) => {}) as React.Dispatch<React.SetStateAction<StudentData[]>>,
 });
 
+/**
+ * Context provider for all students on the queue
+ */
 const AllStudentsContextProvider = ({children}: {children: React.ReactNode}) => {
   const {userData} = useContext(UserDataContext);
   const [allStudents, setAllStudents] = useState<StudentData[]>([]);
 
+  // Load all students if user is a TA and subscribe to changes
   useEffect(() => {
     if (userData.isTA) {
       HomeService.getAllStudents().then((res) => {
