3099f113714a433e80db2dbf5394071611fe0e89
Jackson Romero <jacksontromero@gmail.com>
2023-09-28 19:53:15 -0400

    Add socket resubscription after page refocus if socket closed

--- client/src/services/SocketsService.tsx
+++ client/src/services/SocketsService.tsx
@@ -3,7 +3,9 @@ import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
-let socket;
+let socket = null;
+const subscriptions = [];
+
 const SOCKET_URL = process.env.REACT_APP_PROTOCOL + '://' + process.env.REACT_APP_DOMAIN;
 const SOCKET_PATH = process.env.REACT_APP_SOCKET_PATH;
 
@@ -25,6 +27,9 @@ export const socketSubscribeTo = (emission, callback) => {
     initiateSocket();
   }
 
+  // Add the event and callback to our list of subscriptions
+  subscriptions.push({emission, callback});
+
   socket.on(emission, (data) => {
     callback(data);
   });
@@ -37,3 +42,23 @@ export const socketUnsubscribeFrom = (emission) => {
 
   socket.off(emission);
 };
+
+// Function to resubscribe to all events
+const resubscribeAll = () => {
+  subscriptions.forEach(({emission, callback}) => {
+    socketSubscribeTo(emission, callback);
+  });
+};
+
+// Visibility change listener
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible') {
+    // The page is visible again
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+      // If the socket is not open, reconnect and resubscribe to all events
+      socket = null;
+      initiateSocket();
+      resubscribeAll();
+    }
+  }
+});
