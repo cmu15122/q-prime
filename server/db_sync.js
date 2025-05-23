a38aebcb76d57f5913169a4ca14c87d92a2e713d
Jackson Romero <jacksontromero@gmail.com>
2025-05-23 10:12:04 -0400

    add alter command

--- server/db_sync.js
+++ server/db_sync.js
@@ -1,11 +1,24 @@
 const models = require("./models");
 const config = require("./config/config");
 
+// Parse command line arguments
+const args = process.argv.slice(2);
+let alterOption = false; // default value
+
+for (const arg of args) {
+    if (arg.startsWith('--alter=')) {
+        const value = arg.split('=')[1];
+        alterOption = value === 'true';
+        break;
+    }
+}
+
 models.sequelize.authenticate().then(() => {
     console.log("Connected to the database!");
-    return models.sequelize.sync();
+    console.log(`Syncing with alter option: ${alterOption}`);
+    return models.sequelize.sync({ alter: alterOption });
 }).then(() => {
-    console.log("Synced with the database!");
+    console.log(`Synced with the database! (alter: ${alterOption})`);
 }).catch(err => {
     console.log("An error occurred while syncing the database: ", err);
     process.exit();
